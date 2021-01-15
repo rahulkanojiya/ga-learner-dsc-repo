a9fae8678cb38d4d70afb1ca43ced550c0daa3ed
Rahul kanojiya <rahulkanojiya809@gmail.com>
2021-01-15 19:24:05 +0530

    Code file created.

---
+++ statistics-for-machine-learning/code.py
@@ -0,0 +1,19 @@
+# --------------
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the dataframe
+df = pd.read_csv(path)
+
+#Code starts here
+
+result = False
+bayes = 0.8685
+
+df1 = df[df['paid.back.loan'] == 'No']
+
+
+
+
+
